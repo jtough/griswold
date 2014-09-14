c87541063bd18d010cacfe74ffff19f7728be73d
Jim Tough <jim@jimtough.com>
2014-09-14 11:38:06 -0300

    Making a more interesting login dialog. This revision is a work-in-progress.

--- Griswold/src/main/java/com/jimtough/griswold/AuthenticationController.java
+++ Griswold/src/main/java/com/jimtough/griswold/AuthenticationController.java
@@ -1,5 +1,9 @@
 package com.jimtough.griswold;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,26 +17,33 @@ import com.jimtough.griswold.beans.User;
 import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -42,6 +53,13 @@ public class AuthenticationController {
 
 	private static final Logger logger =
 			LoggerFactory.getLogger(AuthenticationController.class);
+
+	// all text, borders, svg paths will use white
+	private static final Color FOREGROUND_COLOR_FOR_INPUT_CONTROLS = 
+			Color.rgb(255, 255, 255, .9);
+
+	// create a model representing a user
+	private final User user = new User();
 	
 	//private final static String MY_PASS = "xxx";
 	private final static String TEST_PASSWORD = UserAuthenticatorStub.STUB_PASSWORD;
@@ -85,67 +103,48 @@ public class AuthenticationController {
 		this.stage.close();
 	}
 	
-	public void createAuthenticationDialogScene(Stage parent) {
-		
-		// create a model representing a user
-		User user = new User();
+	ImageView createImageView(
+			ReadOnlyDoubleProperty sceneWidthProperty) 
+			throws IOException {
+		ImageView imageView = new ImageView();
+		// Keep the aspect ratio of the original image
+		imageView.setPreserveRatio(true);
+		imageView.fitWidthProperty().bind(sceneWidthProperty);
+		InputStream is = this.getClass().getResourceAsStream(
+				"/login-background-image.jpg");
+		BufferedInputStream bis = new BufferedInputStream(is);
+		Image image = new Image(bis);
+		imageView.setImage(image);
+		bis.close();
+		imageView.setOpacity(0.5);
+		//file:///C:/Documents%20and%20Settings/davris/FileSchemeURIs.doc		
+		return imageView;
+	}
 
-		// create a transparent stage
-		stage.initStyle(StageStyle.TRANSPARENT);
-		stage.initModality(Modality.APPLICATION_MODAL);
-		stage.initOwner(parent);
-		
-		Group root = new Group();
-		Scene scene = new Scene(root, 320, 112, Color.rgb(0, 0, 0, 0));
-		stage.setScene(scene);
-		scene.addEventHandler(KeyEvent.KEY_RELEASED,
-			new EventHandler<KeyEvent>() {
-				@Override
-				public void handle(KeyEvent event) {
-					if (event.getCode().equals(KeyCode.ESCAPE)) {
-						logger.info("Escape key pressed - exiting application");
-						Platform.exit();
-					}
-				}
-			});
-
-		// all text, borders, svg paths will use white
-		Color foregroundColor = Color.rgb(255, 255, 255, .9);
-
-		// rounded rectangular background 
-		Rectangle background = new Rectangle(320, 112);
-		background.setX(0);
-		background.setY(0);
-		background.setArcHeight(15);
-		background.setArcWidth(15);
-		background.setFill(Color.rgb(0, 0, 0, .55));
-		background.setStrokeWidth(1.5);
-		background.setStroke(foregroundColor);
-
-		// a read only field holding the user name.
+	Text createUsernameText() {
 		Text userName = new Text();
+		// a read only field holding the user name.
+		//Text userName = new Text();
 		userName.setFont(Font.font("SanSerif", FontWeight.BOLD, 30));
-		userName.setFill(foregroundColor);
+		userName.setFill(FOREGROUND_COLOR_FOR_INPUT_CONTROLS);
 		userName.setSmooth(true);
 		userName.textProperty().bind(user.userNameProperty());
+		
+		return userName;
+	}
 
-		// wrap text node
-		HBox userNameCell = new HBox();
-		userNameCell.prefWidthProperty().bind(stage.widthProperty().subtract(45));
-		userNameCell.getChildren().add(userName);
+	void createUsernameRow(
+			GridPane gridpane, 
+			Text userName) {
+		TextFlow textFlow = new TextFlow(userName);
+		GridPane.setHalignment(textFlow, HPos.LEFT);
+		gridpane.add(textFlow, 1, 0);
+	}
 
-		// pad lock 
-		SVGPath padLock = new SVGPath();
-		padLock.setFill(foregroundColor);
-		padLock.setContent("M24.875,15.334v-4.876c0-4.894-3.981-8.875-8.875-8.875s-8.875,3.981-8.875,8.875v4.876H5.042v15.083h21.916V15.334H24.875zM10.625,10.458c0-2.964,2.411-5.375,5.375-5.375s5.375,2.411,5.375,5.375v4.876h-10.75V10.458zM18.272,26.956h-4.545l1.222-3.667c-0.782-0.389-1.324-1.188-1.324-2.119c0-1.312,1.063-2.375,2.375-2.375s2.375,1.062,2.375,2.375c0,0.932-0.542,1.73-1.324,2.119L18.272,26.956z");
-
-		// first row 
-		HBox row1 = new HBox();
-		row1.getChildren().addAll(userNameCell, padLock);
-
+	PasswordField createPasswordField() {
+		PasswordField passwordField = new PasswordField();
 		//---------------------------------------------------------------
 		// password text field 
-		PasswordField passwordField = new PasswordField();
 		passwordField.setFont(Font.font("SanSerif", 20));
 		passwordField.setPromptText("Password");
 		passwordField.setStyle("-fx-text-fill:black; "
@@ -158,22 +157,32 @@ public class AuthenticationController {
 				"Heyo! Press Escape key to quit. " + 
 				"Fake password is '" + TEST_PASSWORD + "'.");
 		SVGPath passwordTtIcon = new SVGPath();
-		passwordTtIcon.setFill(foregroundColor);
+		passwordTtIcon.setFill(FOREGROUND_COLOR_FOR_INPUT_CONTROLS);
 		passwordTtIcon.setContent("M16,1.466C7.973,1.466,1.466,7.973,1.466,16c0,8.027,6.507,14.534,14.534,14.534c8.027,0,14.534-6.507,14.534-14.534C30.534,7.973,24.027,1.466,16,1.466z M14.757,8h2.42v2.574h-2.42V8z M18.762,23.622H16.1c-1.034,0-1.475-0.44-1.475-1.496v-6.865c0-0.33-0.176-0.484-0.484-0.484h-0.88V12.4h2.662c1.035,0,1.474,0.462,1.474,1.496v6.887c0,0.309,0.176,0.484,0.484,0.484h0.88V23.622z");
 		passwordTT.setGraphic(passwordTtIcon);
 		passwordField.setTooltip(passwordTT);
 		//---------------------------------------------------------------
 		
 		user.passwordProperty().bind(passwordField.textProperty());
+		
+		passwordField.setBackground(new Background(new BackgroundFill(Color.LAWNGREEN, null, null)));
+		
+		return passwordField;
+	}
 
-		// error icon 
-		SVGPath deniedIcon = new SVGPath();
+	void createPasswordRow(
+			GridPane gridpane,
+			PasswordField passwordField,
+			SVGPath deniedIcon,
+			SVGPath grantedIcon) {
+		
+		// error X icon 
 		deniedIcon.setFill(Color.rgb(255, 0, 0, .9));
 		deniedIcon.setStroke(Color.WHITE);// 
 		deniedIcon.setContent("M24.778,21.419 19.276,15.917 24.777,10.415 21.949,7.585 16.447,13.087 10.945,7.585 8.117,10.415 13.618,15.917 8.116,21.419 10.946,24.248 16.447,18.746 21.948,24.248z");
 		deniedIcon.setVisible(false);
 
-		SVGPath grantedIcon = new SVGPath();
+		// checkmark icon
 		grantedIcon.setFill(Color.rgb(0, 255, 0, .9));
 		grantedIcon.setStroke(Color.WHITE);// 
 		grantedIcon.setContent("M2.379,14.729 5.208,11.899 12.958,19.648 25.877,6.733 28.707,9.561 12.958,25.308z");
@@ -184,21 +193,69 @@ public class AuthenticationController {
 		accessIndicator.setAlignment(Pos.CENTER_RIGHT);
 
 		grantedIcon.visibleProperty().bind(GRANTED_ACCESS);
+		
+		GridPane.setHalignment(passwordField, HPos.LEFT);
+		gridpane.add(passwordField, 1, 1);
+		GridPane.setHalignment(accessIndicator, HPos.RIGHT);
+		gridpane.add(accessIndicator, 2, 1);
+	}
+	
+	public void createAuthenticationDialogScene(Stage parent) {
+		// create a transparent stage
+		stage.initStyle(StageStyle.TRANSPARENT);
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.initOwner(parent);
+		stage.setResizable(false);
+		
+		Group root = new Group();
+		Scene scene = new Scene(root, 640, 800, Color.rgb(0, 0, 0, 0));
+		stage.setScene(scene);
+		scene.addEventHandler(KeyEvent.KEY_RELEASED,
+			new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					if (event.getCode().equals(KeyCode.ESCAPE)) {
+						logger.info("Escape key pressed - exiting application");
+						Platform.exit();
+					}
+				}
+			});
 
-		// second row
-		HBox row2 = new HBox(3);
-		row2.getChildren().addAll(passwordField, accessIndicator);
-		HBox.setHgrow(accessIndicator, Priority.ALWAYS);
+		GridPane gridpaneX = new GridPane();
+		gridpaneX.setPadding(new Insets(5));
+		gridpaneX.setHgap(5);
+		gridpaneX.setVgap(5);
+		ColumnConstraints col1Size = new ColumnConstraints(8);
+		ColumnConstraints col2Size = new ColumnConstraints(550);
+		ColumnConstraints col3Size = new ColumnConstraints(32);
+		gridpaneX.getColumnConstraints().addAll(col1Size, col2Size, col3Size);
+		//gridpaneX.setGridLinesVisible(false);
+		gridpaneX.setBackground(new Background(new BackgroundFill(Color.SALMON, null, null)));
+		gridpaneX.setGridLinesVisible(true);
+
+		
+		//// all text, borders, svg paths will use white
+		//Color foregroundColor = Color.rgb(255, 255, 255, .9);
+
+		ImageView imageView = null;
+		try {
+			imageView = createImageView(scene.widthProperty());
+		} catch (Exception e) {
+			throw new RuntimeException("Cannot load image", e);
+		}
+		
+		// TODO Replace Text with TextField
+		Text userName = createUsernameText();
+		createUsernameRow(gridpaneX, userName);
+		
+		PasswordField passwordField = createPasswordField();
+		SVGPath deniedIcon = new SVGPath();
+		SVGPath grantedIcon = new SVGPath();
+		
+		createPasswordRow(gridpaneX, passwordField, deniedIcon, grantedIcon);
 
 		// user hits the enter key
 		passwordField.setOnAction(actionEvent -> {
-			//if (GRANTED_ACCESS.get()) {
-			//	logger.info("User is granted access.");
-			//	this.onSuccessfulAuthentication();
-			//	//Platform.exit();
-			//} else {
-			//	deniedIcon.setVisible(true); 
-			//}
 			Credentials credentials = new Credentials(
 					user.userNameProperty().get(), 
 					user.passwordProperty().get());
@@ -207,14 +264,14 @@ public class AuthenticationController {
 				AuthenticatedUser authUser = 
 						userAuthenticator.authenticate(credentials);
 				setAuthUser(authUser);
+				logger.info("User is granted access.");
+				this.onSuccessfulAuthentication(getAuthUser());
 			} catch (AuthenticationFailureException e) {
-				logger.info("Authentication failed", e);
+				logger.info("Authentication failed | " + e.getMessage());
 				deniedIcon.setVisible(true);
 				ATTEMPTS.set(ATTEMPTS.add(1).get());
 				logger.info("Attempts: " + ATTEMPTS.get());
 			}
-			logger.info("User is granted access.");
-			this.onSuccessfulAuthentication(getAuthUser());
 		});
 
 		// listener when the user types into the password field
@@ -225,7 +282,8 @@ public class AuthenticationController {
 				deniedIcon.setVisible(false);
 			}
 		});
-
+		
+		
 		// listener on number of attempts
 		ATTEMPTS.addListener((obs, ov, nv) -> {
 			if (MAX_ATTEMPTS == nv.intValue()) {
@@ -236,11 +294,9 @@ public class AuthenticationController {
 		});
 
 		VBox formLayout = new VBox(4);
-		formLayout.getChildren().addAll(row1, row2);
-		formLayout.setLayoutX(12);
-		formLayout.setLayoutY(12);
+		formLayout.getChildren().addAll(imageView, gridpaneX);
 		
-		root.getChildren().addAll(background, formLayout);
+		root.getChildren().addAll(formLayout);
 
 		stage.show();
 
